27ed954c5a60a33c85b55617b3323c532b3cb0e7
M4TH3US17 <matheusdalvino50@gmail.com>
2022-04-20 07:10:18 -0400

    Projeto Criado

---
+++ src/test/java/com/prova/azship/AdmAzsShipping2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prova.azship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmAzsShipping2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
